a4dfd4eb9cc23da23473e3899ae12caf6675ba44
dan <dan.gora2004@gmail.com>
2025-02-28 19:19:28 +0200

    fix

--- src/MapPage.js
+++ src/MapPage.js
@@ -4,7 +4,7 @@ const MapPage = () => {
   useEffect(() => {
     const loadGoogleMaps = () => {
       const script = document.createElement("script");
-      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyARbbRhFgPizWk1ozxKn1yDyX6jJUrUTMo&callback=initMap`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=&callback=initMap`;
       script.async = true;
       script.defer = true;
       document.head.appendChild(script);
